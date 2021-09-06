c597a7e6c2b18a20e4e4cf751ee49d2d4d70fb25
Sid <sidchopp@gmail.com>
2021-09-05 23:00:41 -0600

    Collapse included

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,9 +17,12 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+
+
 import clsx from 'clsx';
 import IconButton from '@material-ui/core/IconButton';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
 
 // components
 
@@ -173,6 +176,9 @@ export default function Projects() {
                         <ExpandMoreIcon />
                       </IconButton>
                     </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                      <CardContent>Hi</CardContent>
+                    </Collapse>
                   </Card>
                 </Grid>
               )
