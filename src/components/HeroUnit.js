94dc265fbb84f3fddc0b50c6c9210878bb59c46b
Sid <sidchopp@gmail.com>
2021-09-25 20:43:25 -0600

    backround image changed in hero page

--- src/components/HeroUnit.js
+++ src/components/HeroUnit.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
-    backgroundImage: `url(https://images.unsplash.com/photo-1548685913-fe6678babe8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1138&q=80)`,
+    backgroundImage: `url(https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80)`,
   },
   heroButtons: {
     marginTop: theme.spacing(4),
