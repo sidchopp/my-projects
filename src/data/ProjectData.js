63f4cd7b95ca15454029583b62354a77667f844b
Sid <sidchopp@gmail.com>
2021-10-09 10:02:36 -0600

    reordering of projects

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,71 +1,28 @@
 const ProjectData = [
   {
-    "id": "1",
-    "order": 3,
-    "title": "Fusion Alberta",
-    "bestView": 'Big Screens and Laptops only',
-    "dates": "December 2020 - February 2021",
-    "duties": [
-      "Fusion is a networking platform that provides a centralized database of tech events in Alberta. It was my final project at InceptionU Ltd.(EvolveU-Full Stack Program) which I worked on with a team of three other learners."
-    ],
-    'img': "https://images.unsplash.com/photo-1551818255-e6e10975bc17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1225&q=80",
-    "website": "https://fusion-alberta.herokuapp.com/",
-    "techUsed": "MERN Stack, Firebase, Semantic UI React, Heroku, EmailJS.",
-    "company": "InceptionU Ltd."
-  },
-  {
-    "id": "2",
+    "id": "8",
     "order": 1,
-    "title": "Rolling Dice",
-    "bestView": 'Big Screens and Laptops only',
-    "dates": "July 2021 - July 2021",
-    "duties": [
-      "An exciting game in which two players throw a dice, till one of them scores a 50 and WINS!!"
-    ],
-    'img': "https://images.unsplash.com/photo-1581788927061-ac0e09850ed0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1568&q=80",
-    "website": "https://sid-rolling-dice.netlify.app/",
-    "techUsed": "HTML, CSS, JavaScript",
-    "company": "SID"
-  },
-  {
-    "id": "3",
-    "order": 3,
-    "bestView": 'On ALL Big Screens, Laptops, Tablets, Mobiles(Horizontal position better than Vertical)',
-    "title": "Periodic Table",
-    "dates": "September 2021 - September 2021",
-    "duties": [
-      "The periodic table is a tabular display of the chemical elements. It is widely used in Chemistry, Physics, and other sciences, and is generally seen as an icon of Chemistry."
-    ],
-    'img': "https://media.istockphoto.com/photos/silicon-element-of-mendeleev-periodic-table-magnified-with-magnifier-picture-id511921456?b=1&k=20&m=511921456&s=170667a&w=0&h=H6eAgLsAjiv2hdmLlx5fxfyp9aHwp4TOskDsq49B3vk=",
-    "website": "https://sid-periodic-table.netlify.app/",
-    "techUsed": "Node.js, React, Semantic UI React",
-    "company": "SID"
-  },
-  {
-    "id": "4",
-    "order": 1,
-    "title": "My Projects",
+    "title": "Share",
     "bestView": 'On ALL Big Screens, Laptops, Tablets, Mobiles',
-    "dates": "August 2021 - September 2021",
+    "dates": "September 2021 - present",
     "duties": [
-      "You are watching this project in action at the moment. It is a compilation of all the Projects that i have successfully completed till date."
+      "I am currently working on this amazing project.Will update shortly...."
     ],
-    'img': "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
-    "website": "https://sid-projects.netlify.app/",
-    "techUsed": "Node.js, React, MUI, Semantic UI React, EmailJS",
+    'img': "https://images.unsplash.com/photo-1482731215275-a1f151646268?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    "techUsed": "MERN Stack",
     "company": "SID"
   },
   {
-    "id": "5",
+    "id": "7",
     "order": 1,
+    "title": "Seasons",
     "bestView": 'On ALL Big Screens, Laptops, Tablets, Mobiles',
-    "title": "About My Location",
-    "dates": "September 2021 - September 2021",
+    "dates": "October 2021 - October 2021",
     "duties": [
-      "This App will not only tell about the exact location of the user, but also provide a lot of other useful info about that country."
+      "This App will show the season (Spring, Summer, Winter or Fall) based on the User's location on the globe. It also displays the Latitude and Longitude of that location."
     ],
-    'img': "https://images.unsplash.com/photo-1519500099198-fd81846b8f03?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    "website": "https://about-my-location.netlify.app/",
+    'img': "https://images.unsplash.com/photo-1470347963396-b4fab6f71f55?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80",
+    "website": "https://sid-seasons.netlify.app/",
     "techUsed": "Node.js, React, Semantic UI React",
     "company": "SID"
   },
@@ -84,32 +41,81 @@ const ProjectData = [
     "company": "SID"
   },
   {
-    "id": "7",
+    "id": "5",
     "order": 1,
-    "title": "Seasons",
     "bestView": 'On ALL Big Screens, Laptops, Tablets, Mobiles',
-    "dates": "October 2021 - October 2021",
+    "title": "About My Location",
+    "dates": "September 2021 - September 2021",
     "duties": [
-      "This App will show the season (Spring, Summer, Winter or Fall) based on the User's location on the globe. It also displays the Latitude and Longitude of that location."
+      "This App will not only tell about the exact location of the user, but also provide a lot of other useful info about that country."
     ],
-    'img': "https://images.unsplash.com/photo-1470347963396-b4fab6f71f55?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80",
-    "website": "https://sid-seasons.netlify.app/",
+    'img': "https://images.unsplash.com/photo-1519500099198-fd81846b8f03?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    "website": "https://about-my-location.netlify.app/",
     "techUsed": "Node.js, React, Semantic UI React",
     "company": "SID"
   },
   {
-    "id": "8",
+    "id": "4",
     "order": 1,
-    "title": "Share",
+    "title": "My Projects",
     "bestView": 'On ALL Big Screens, Laptops, Tablets, Mobiles',
-    "dates": "September 2021 - present",
+    "dates": "August 2021 - September 2021",
     "duties": [
-      "I am currently working on this amazing project.Will update shortly...."
+      "You are watching this project in action at the moment. It is a compilation of all the Projects that i have successfully completed till date."
     ],
-    'img': "https://images.unsplash.com/photo-1482731215275-a1f151646268?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    "techUsed": "MERN Stack",
+    'img': "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    "website": "https://sid-projects.netlify.app/",
+    "techUsed": "Node.js, React, MUI, Semantic UI React, EmailJS",
     "company": "SID"
   },
+  {
+    "id": "3",
+    "order": 3,
+    "bestView": 'On ALL Big Screens, Laptops, Tablets, Mobiles(Horizontal position better than Vertical)',
+    "title": "Periodic Table",
+    "dates": "September 2021 - September 2021",
+    "duties": [
+      "The periodic table is a tabular display of the chemical elements. It is widely used in Chemistry, Physics, and other sciences, and is generally seen as an icon of Chemistry."
+    ],
+    'img': "https://media.istockphoto.com/photos/silicon-element-of-mendeleev-periodic-table-magnified-with-magnifier-picture-id511921456?b=1&k=20&m=511921456&s=170667a&w=0&h=H6eAgLsAjiv2hdmLlx5fxfyp9aHwp4TOskDsq49B3vk=",
+    "website": "https://sid-periodic-table.netlify.app/",
+    "techUsed": "Node.js, React, Semantic UI React",
+    "company": "SID"
+  },
+
+  {
+    "id": "2",
+    "order": 1,
+    "title": "Rolling Dice",
+    "bestView": 'Big Screens and Laptops only',
+    "dates": "July 2021 - July 2021",
+    "duties": [
+      "An exciting game in which two players throw a dice, till one of them scores a 50 and WINS!!"
+    ],
+    'img': "https://images.unsplash.com/photo-1581788927061-ac0e09850ed0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1568&q=80",
+    "website": "https://sid-rolling-dice.netlify.app/",
+    "techUsed": "HTML, CSS, JavaScript",
+    "company": "SID"
+  },
+  {
+    "id": "1",
+    "order": 3,
+    "title": "Fusion Alberta",
+    "bestView": 'Big Screens and Laptops only',
+    "dates": "December 2020 - February 2021",
+    "duties": [
+      "Fusion is a networking platform that provides a centralized database of tech events in Alberta. It was my final project at InceptionU Ltd.(EvolveU-Full Stack Program) which I worked on with a team of three other learners."
+    ],
+    'img': "https://images.unsplash.com/photo-1551818255-e6e10975bc17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1225&q=80",
+    "website": "https://fusion-alberta.herokuapp.com/",
+    "techUsed": "MERN Stack, Firebase, Semantic UI React, Heroku, EmailJS.",
+    "company": "InceptionU Ltd."
+  },
+
+
+
+
+
 
 ];
 
