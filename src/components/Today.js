acf4b38e46e04866dd28801dde928092eccd9bcb
Sid <sidchopp@gmail.com>
2021-10-29 14:09:08 -0600

    Today component created

---
+++ src/components/Today.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Today() {
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return (
+    <div>
+      Hope you are enjoying your <i>{days[new Date().getDay()]} !</i>
+    </div>
+  )
+}
+
+export default Today;
+
