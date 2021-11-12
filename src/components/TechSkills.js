04a06ba3fff652e0222ef95f09b5fb6a41d4b54d
Sid <sidchopp@gmail.com>
2021-11-11 18:11:24 -0700

    TechSkills modified with some info and Popups

--- src/components/TechSkills.js
+++ src/components/TechSkills.js
@@ -1,40 +1,94 @@
 import React from 'react'
-import { Icon, Grid, Container, Header } from 'semantic-ui-react'
+import { Icon, Grid, Container, Header, Popup } from 'semantic-ui-react'
+
+//CSS
+import '../App.css'
 
 function TechSkills() {
   return (
-    <Container >
-      <Grid doubling>
+    <Container  >
+      <Header as="h2" textAlign='center'>
+        <span className="font-resume"> Technologies, I  Love to work with</span>
+      </Header>
+      <Grid doubling style={{ marginTop: '10px' }}>
         <Grid.Row columns={6} id='db_1'>
           <Grid.Column>
-            <Header as="h1">
-              <Icon name="react"></Icon>
-            </Header>
+            <Popup
+              inverted
+              trigger={<Header as="h1">
+                <Icon
+                  link
+                  name="react"
+                />
+              </Header>}
+              content='React'
+              position='bottom center'
+            />
           </Grid.Column>
           <Grid.Column>
-            <Header as="h1">
-              <Icon name="node"></Icon>
-            </Header>
+            <Popup
+              inverted
+              trigger={<Header as="h1">
+                <Icon
+                  link
+                  name="node"
+                />
+              </Header>}
+              content='Node'
+              position='bottom center'
+            />
           </Grid.Column>
           <Grid.Column>
-            <Header as="h1">
-              <Icon name="js square"></Icon>
-            </Header>
+            <Popup
+              inverted
+              trigger={<Header as="h1">
+                <Icon
+                  link
+                  name="js square"
+                />
+              </Header>}
+              content='JavaScript'
+              position='bottom center'
+            />
           </Grid.Column>
           <Grid.Column>
-            <Header as="h1">
-              <Icon name="html5"></Icon>
-            </Header>
+            <Popup
+              inverted
+              trigger={<Header as="h1">
+                <Icon
+                  link
+                  name="html5"
+                />
+              </Header>}
+              content='HTML 5'
+              position='bottom center'
+            />
           </Grid.Column>
           <Grid.Column>
-            <Header as="h1">
-              <Icon name="css3 alternate"></Icon>
-            </Header>
+            <Popup
+              inverted
+              trigger={<Header as="h1">
+                <Icon
+                  link
+                  name="css3 alternate"
+                />
+              </Header>}
+              content='CSS 3'
+              position='bottom center'
+            />
           </Grid.Column>
           <Grid.Column>
-            <Header as="h1">
-              <Icon name="aws"></Icon>
-            </Header>
+            <Popup
+              inverted
+              trigger={<Header as="h1">
+                <Icon
+                  link
+                  name="aws"
+                />
+              </Header>}
+              content='Amazon Web Services'
+              position='bottom center'
+            />
           </Grid.Column>
         </Grid.Row>
       </Grid>
