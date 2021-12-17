39f504f7aee178522aa01e48ddfd23a90d1a0c61
Sid <sidchopp@gmail.com>
2021-12-17 15:54:44 -0700

    Icons in techskills refactored and popup removed from them

--- src/components/TechSkills.js
+++ src/components/TechSkills.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon, Grid, Container, Header, Popup } from 'semantic-ui-react'
+import { Icon, Grid, Container, Header } from 'semantic-ui-react'
 
 //CSS
 import '../App.css'
@@ -13,91 +13,43 @@ function TechSkills() {
       <Grid columns={6} divided doubling style={{ marginTop: '10px' }}>
         <Grid.Row id='db_1'>
           <Grid.Column>
-            <Popup
-              inverted
-              mouseEnterDelay={500}
-              mouseLeaveDelay={500}
-              on='hover'
-              trigger={<Header as="h1">
-                <a href='https://reactjs.org/' target="_blank" rel="noreferrer">
-                  <Icon size="large" color='grey' name="react" />
-                </a>
-              </Header>}
-              content='React'
-              position='bottom center'
-            />
+            <Header as="h1">
+              <a href='https://reactjs.org/' target="_blank" rel="noreferrer">
+                <Icon size="large" style={{ color: "#61dafb" }} name="react" />
+              </a>
+            </Header>
           </Grid.Column>
           <Grid.Column>
-            <Popup
-              inverted
-              mouseEnterDelay={500}
-              mouseLeaveDelay={500}
-              on='hover'
-              trigger={<Header as="h1">
-                <a href='https://nodejs.org/en/' target="_blank" rel="noreferrer">
-                  <Icon size="large" color='grey' name="node" />
-                </a>
-              </Header>}
-              content='Node'
-              position='bottom center'
-            />
+            <Header as="h1">
+              <a href='https://nodejs.org/en/' target="_blank" rel="noreferrer">
+                <Icon size="large" style={{ color: "#68A063" }} name="node" />
+              </a>
+            </Header>
           </Grid.Column>
           <Grid.Column>
-            <Popup
-              inverted
-              mouseEnterDelay={500}
-              mouseLeaveDelay={500}
-              on='hover'
-              trigger={<Header as="h1">
-                <a href='https://aws.amazon.com/' target="_blank" rel="noreferrer">
-                  <Icon size="large" color='grey' name="aws" />
-                </a>
-              </Header>}
-              content='Amazon Web Services'
-              position='bottom center'
-            />
+            <Header as="h1">
+              <a href='https://aws.amazon.com/' target="_blank" rel="noreferrer">
+                <Icon size="large" style={{ color: " #FF9900" }} name="aws" />
+              </a>
+            </Header>
           </Grid.Column>
           <Grid.Column>
-            <Popup
-              mouseEnterDelay={500}
-              mouseLeaveDelay={500}
-              on='hover'
-              inverted
-              trigger={<Header as="h1">
-                <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript' target="_blank" rel="noreferrer"> <Icon size="large" color='grey' name="js square" />
-                </a>
-              </Header>}
-              content='JavaScript'
-              position='bottom center'
-            />
+            <Header as="h1">
+              <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript' target="_blank" rel="noreferrer"> <Icon size="large" style={{ color: "#323330" }} name="js square" />
+              </a>
+            </Header>
           </Grid.Column>
           <Grid.Column>
-            <Popup
-              inverted
-              mouseEnterDelay={500}
-              mouseLeaveDelay={500}
-              on='hover'
-              trigger={<Header as="h1">
-                <a href='https://developer.mozilla.org/en-US/docs/Web/HTML' target="_blank" rel="noreferrer"> <Icon size="large" color='grey' name="html5" />
-                </a>
-              </Header>}
-              content='HTML5'
-              position='bottom center'
-            />
+            <Header as="h1">
+              <a href='https://developer.mozilla.org/en-US/docs/Web/HTML' target="_blank" rel="noreferrer"> <Icon size="large" style={{ color: "#f06529" }} name="html5" />
+              </a>
+            </Header>
           </Grid.Column>
           <Grid.Column>
-            <Popup
-              inverted
-              mouseEnterDelay={500}
-              mouseLeaveDelay={500}
-              on='hover'
-              trigger={<Header as="h1">
-                <a href='https://developer.mozilla.org/en-US/docs/Web/CSS' target="_blank" rel="noreferrer"> <Icon size="large" color='grey' name="css3 alternate" />
-                </a>
-              </Header>}
-              content='CSS3'
-              position='bottom center'
-            />
+            <Header as="h1">
+              <a href='https://developer.mozilla.org/en-US/docs/Web/CSS' target="_blank" rel="noreferrer"> <Icon size="large" style={{ color: "#264de4" }} name="css3 alternate" />
+              </a>
+            </Header>
           </Grid.Column>
         </Grid.Row>
       </Grid>
