a2c744435fc0cad563e453eb02a13827360da11b
Sid <sidchopp@gmail.com>
2022-01-28 15:10:30 -0700

    lazyloading of Projects

--- src/components/ProjectsCards.js
+++ src/components/ProjectsCards.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import LazyLoad from 'react-lazyload';
 import Button from '@material-ui/core/Button';
 import TouchAppIcon from '@material-ui/icons/TouchApp';
 import Card from '@material-ui/core/Card';
@@ -29,69 +30,72 @@ function ProjectsCards() {
   };
   return (
     <Container className={classes.cardGrid} maxWidth="lg">
-      <Paper sx={{ backgroundColor: "#4cbfa6" }} className={classes.projectPaper} elevation={4} >
-        <Container className={classes.myProjectsHeader} maxWidth="lg">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            gutterBottom
-          >
-            <span className='font-projects' >My Projects</span>
-          </Typography>
-          {/* <Icon className='arrow-animation' size='big' name='chevron down' /> */}
-        </Container>
-        <Grid style={{ marginTop: '10px' }} container spacing={4}>
-          {ProjectData.map((card) => {
-            const { title, duties, techUsed, website, img, bestView, typeOfProject } = card
-            return (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardHeader
-                    action={typeOfProject === "Individual" ? <PersonIcon /> : <GroupIcon />}
-                    title={title}
-                    subheader={techUsed}
+      <LazyLoad height={1200} offset={[-150, 0]} >
+        <Paper sx={{ backgroundColor: "#4cbfa6" }} className={classes.projectPaper} elevation={4} >
+          <Container className={classes.myProjectsHeader} maxWidth="lg">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              gutterBottom
+            >
+              <span className='font-projects' >My Projects</span>
+            </Typography>
+            {/* <Icon className='arrow-animation' size='big' name='chevron down' /> */}
+          </Container>
+          <Grid style={{ marginTop: '10px' }} container spacing={4}>
 
-                  />
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={img}
-                    alt={title}
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography>
-                      {duties}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button variant="outlined" color="inherit" href={website} target="_blank" >
-                      <TouchAppIcon /> View
-                    </Button>
-                    <IconButton
-                      style={{ marginLeft: 'auto' }}
-                      className={clsx(classes.expand, {
-                        [classes.expandOpen]: expanded,
-                      })}
-                      onClick={handleExpandClick}
-                      aria-expanded={expanded}
-                      aria-label="show more"
-                    >
-                      {/* To change the icon with each click depending on state */}
-                      {/* {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />} */}
-                      <ExpandMoreIcon />
-                    </IconButton>
-                  </CardActions>
-                  {/* Iff only showInfo is true, only then show info, use AND operator */}
-                  {expanded && <CardContent>
-                    <div style={{ margin: '3px' }}> <h4> Best View: </h4> {bestView}</div>
-                    {/* <div style={{ margin: '3px' }}> <h4>Made from: </h4>{dates}</div> */}
-                  </CardContent>}
-                </Card>
-              </Grid>
-            )
-          })}
-        </Grid>
-      </Paper>
+            {ProjectData.map((card) => {
+              const { title, duties, techUsed, website, img, bestView, typeOfProject } = card
+              return (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardHeader
+                      action={typeOfProject === "Individual" ? <PersonIcon /> : <GroupIcon />}
+                      title={title}
+                      subheader={techUsed}
+
+                    />
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image={img}
+                      alt={title}
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography>
+                        {duties}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button variant="outlined" color="inherit" href={website} target="_blank" >
+                        <TouchAppIcon /> View
+                      </Button>
+                      <IconButton
+                        style={{ marginLeft: 'auto' }}
+                        className={clsx(classes.expand, {
+                          [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                      >
+                        {/* To change the icon with each click depending on state */}
+                        {/* {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />} */}
+                        <ExpandMoreIcon />
+                      </IconButton>
+                    </CardActions>
+                    {/* Iff only showInfo is true, only then show info, use AND operator */}
+                    {expanded && <CardContent>
+                      <div style={{ margin: '3px' }}> <h4> Best View: </h4> {bestView}</div>
+                      {/* <div style={{ margin: '3px' }}> <h4>Made from: </h4>{dates}</div> */}
+                    </CardContent>}
+                  </Card>
+                </Grid>
+              )
+            })}
+          </Grid>
+        </Paper>
+      </LazyLoad>
     </Container>
   )
 }
