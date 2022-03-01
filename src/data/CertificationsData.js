b212c6fde71be7cc3c43c9ac0110deb4ef7f616d
Sid <sidchopp@gmail.com>
2022-03-01 15:27:48 -0700

    slight trimming in skills learned section of badges comp

--- src/data/CertificationsData.js
+++ src/data/CertificationsData.js
@@ -10,7 +10,7 @@ const certificationsData = [
     "issuedBy": "Amazon Web Services Training and Certification",
     "validity": "Feb 2022 - Feb 2025",
     "skills": [
-      "Amazon Web Services", "AWS Certification", "AWS Cloud", "Code Deployment", "Code Development"
+      "Amazon Web Services", "AWS Cloud", "Code Deployment", "Code Development"
     ],
     "img": `${awsDev}`,
     "website": "https://www.credly.com/badges/73443cb2-75ab-4fce-97b6-771d7be5912a?source=linked_in_profile"
@@ -21,7 +21,7 @@ const certificationsData = [
     "issuedBy": "Amazon Web Services Training and Certification",
     "validity": "July 2021 - Feb 2025",
     "skills": [
-      "Amazon Web Services", "AWS Certification", "AWS Cloud", "Cloud Computing", "Cloud Services"
+      "Amazon Web Services", "AWS Cloud", "Cloud Computing", "Cloud Services"
     ],
     "img": `${awsCP}`,
     "website": "https://www.credly.com/badges/86e0a91c-4f81-4915-b135-3a527e56e9da/public_url"
@@ -32,7 +32,7 @@ const certificationsData = [
     "issuedBy": "Amazon Web Services Training and Certification",
     "validity": "Feb 2022 - Feb 2025",
     "skills": [
-      "Cloud Native", "Devops", "Iaas PaaS Saas", "Hybrid Multicloud", "Cloud Computing"
+      "Cloud Native", "Devops", "Iaas PaaS Saas", "Hybrid Multicloud"
     ],
     "img": `${ibmCloud}`,
     "website": "https://www.coursera.org/account/accomplishments/verify/HASHBWKTZQG8?utm_source%3Dandroid%26utm_medium%3Dcertificate%26utm_content%3Dcert_image%26utm_campaign%3Dsharing_cta%26utm_product%3Dcourse"
