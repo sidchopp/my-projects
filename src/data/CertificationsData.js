30df78c2d5a906ad02a82327d8228d481b74c90f
Sid <sidchopp@gmail.com>
2022-03-19 12:22:55 -0600

    UOA SPM certifications1to3 included

--- src/data/CertificationsData.js
+++ src/data/CertificationsData.js
@@ -2,6 +2,7 @@
 import awsDev from '../images/awsDev.png';
 import awsCP from '../images/awsCP.png';
 import ibmCloud from '../images/ibmCloud.png';
+import UOA from '../images/UOA.png';
 
 const certificationsData = [
   {
@@ -37,6 +38,47 @@ const certificationsData = [
     "img": `${ibmCloud}`,
     "website": "https://www.coursera.org/account/accomplishments/verify/HASHBWKTZQG8?utm_source%3Dandroid%26utm_medium%3Dcertificate%26utm_content%3Dcert_image%26utm_campaign%3Dsharing_cta%26utm_product%3Dcourse"
   },
+  {
+    "id": "04",
+    "title": "Introduction to Software Product Management",
+    "issuedBy": "University of Alberta",
+    "validity": "Always",
+    "skills": [
+      "Relate software product management to better software products",
+      "Recognize the role of a software product manager",
+      "Reflect on how Agile principles will improve your own projects",
+    ],
+    "img": `${UOA}`,
+    "website": "https://coursera.org/share/f031e2f38e47c8c224d8e5068e3d5176"
+  },
+  {
+    "id": "05",
+    "title": "Software Processes and Agile Practices",
+    "issuedBy": "University of Alberta",
+    "validity": "Always",
+    "skills": [
+      "Distinguish between different process models for organizing software production",
+      "Gauge the applicability of process models for a software development project",
+      "Apply the fundamentals of Agile software development and management practices",
+    ],
+    "img": `${UOA}`,
+    "website": "https://coursera.org/share/7c6a79ddcccca270e718ffbfe183cd95"
+  },
+  {
+    "id": "06",
+    "title": "Client Needs and Software Requirements",
+    "issuedBy": "University of Alberta",
+    "validity": "Always",
+    "skills": [
+      "Create clear requirements to drive effective software development",
+      "Visualize client needs using low-fidelity prototypes",
+      "Maximize the effectiveness of client interactions",
+      "Adapt to changing product requirements"
+    ],
+    "img": `${UOA}`,
+    "website": "https://coursera.org/share/d360b0a443bf8da365ca29b5c1137340"
+  },
+
 
 ];
 
