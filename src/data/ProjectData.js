16d099be119573c041d1c90165acfb56998d116e
Sid <sidchopp@gmail.com>
2022-03-29 13:47:17 -0600

    AWS auth details modified

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -6,7 +6,7 @@ const ProjectData = [
     "title": "Authentication & Authorization",
     "dates": "February 2021 - February 2022",
     "duties": [
-      "This App uses AWS Amplify for UI & AWS Cognito for Authentication/Authorization."
+      "It uses AWS Cognito for Authentication & Authorization, AWS Amplify for CDN, deployment."
     ],
     'img': "https://images.unsplash.com/photo-1607968980162-8cdb8f0dae6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
     "website": "https://main.d30a6z92p9jj0o.amplifyapp.com/",
